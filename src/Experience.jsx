678ac9529a62f1629299e70171981f122ca805c5
jawid-fakhari <jawid_fakhari@live.it>
2024-10-16 15:26:58 +0200

    event handlre

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,33 +2,35 @@ import { useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 
-export default function Experience()
-{
+export default function Experience() {
     const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
+
+    useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2
     })
+
+    const eventHandlre = () => {
+        cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
+    }
 
     return <>
 
         <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+        <directionalLight position={[1, 2, 3]} intensity={4.5} />
+        <ambientLight intensity={1.5} />
 
-        <mesh position-x={ - 2 }>
+        <mesh position-x={- 2}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={cube} position-x={2} scale={1.5} onClick={eventHandlre}>
             <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+            <meshStandardMaterial />
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
